5da13d1507e5c72a69761cfbc4e7ceee708bd03b
Myo Thant <jamesrobinson636478@gmail.com>
2023-10-08 23:52:34 +0630

    Add Blog Update Feature

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, BlogCreateView
+from .views import HomeView, BlogCreateView, BlogDetailView
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='blog.home'),
     path('new-blog/', BlogCreateView.as_view(), name='blog.new'),
+    path('update-blog/<pk>/', BlogDetailView.as_view(), name='blog.update'),
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, View
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import ListView, View, DetailView
+from django.views.generic.edit import ModelFormMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Blog
 from .forms import BlogCreateForm
@@ -26,3 +27,27 @@ class BlogCreateView(View, LoginRequiredMixin):
             Blog.objects.create(**data)
             return redirect('blog.home')
         return render(request, 'createBlog.html', {'form': form})
+    
+class BlogDetailView(ModelFormMixin, DetailView, LoginRequiredMixin):
+    login_url = '/account/login/'
+    template_name = 'updateBlog.html'
+    success_url = '/blog/home'
+    model = Blog
+    form_class = BlogCreateForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        init_obj = self.get_object()
+        context['form'] = BlogCreateForm(instance=init_obj)
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        blog_obj = self.get_object()
+        form = BlogCreateForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+            blog_obj.title, blog_obj.body = title, body
+            blog_obj.save()
+            return redirect('blog.home')
+        return redirect('blog.update')
