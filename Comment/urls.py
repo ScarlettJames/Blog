129c43a42827e822d4ca2717658815fadbbfbb46
Scarlett <scarlettjames164@gmail.com>
2023-11-19 10:08:33 +0630

    fix url {Typeerror)

--- Comment/urls.py
+++ Comment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import NewComment
 
-urlpatterns = {
+urlpatterns = [
     path('new', NewComment.as_view(), name='comment.new'),
-}
+]
