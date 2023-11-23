451b8c87375630da6792044b8d8f5383f34aae70
Scarlett <scarlettjames164@gmail.com>
2023-11-23 13:05:56 +0630

    Add Comment Feature

--- Comment/views.py
+++ Comment/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import CommentModel
+from django.contrib.auth.models import User
+from Blog.models import Blog
+from django.http import JsonResponse
+from json import dumps
 
 # Create your views here.
 class NewComment(CreateView):
     model = CommentModel
     fields = "__all__"
 
-class CommentList(ListView):
-    model = CommentModel
-    template_name = "detailBlog.html"
-    context_object_name = 'comments'
+    def post(self, request, *args, **kwargs):
+        data = dict(request.POST)
+        comment = data['comment'][0]
+        user = User.objects.get(pk = int(data['author'][0]))
+        post = Blog.objects.get(pk = int(data['post'][0]))
+        modelCreated = self.model.objects.create(author = user, post = post, comment = comment)
+        return JsonResponse({'success': True})
